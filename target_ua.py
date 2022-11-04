554043e845b60cb1e5aaa38467448150d9a28a90
rad1an <rad1an.pm@proton.me>
2022-11-04 20:18:12 +0200

    Created a target_ua file

---
+++ target_ua.py
@@ -0,0 +1,7 @@
+"""
+A target game:
+    - A field of 5 letters is generated
+    - The user enters words that has the first letter from the field
+        and is <= 5 chars
+    - The user enters words until he enters ctrl+d
+"""
