31df5ed4fc7b93f1968a885569d455194db2690c
rad1an <rad1an.pm@proton.me>
2022-11-04 20:19:48 +0200

    Implemented generation of the field

--- target_ua.py
+++ target_ua.py
@@ -5,3 +5,18 @@ A target game:
         and is <= 5 chars
     - The user enters words until he enters ctrl+d
 """
+import random
+
+
+def generate_grid() -> list[list[str]]:
+    """
+    Generate a grid of 5 unique ukrainian letters
+
+    Returns
+    -------
+    list[str]
+        The generated grid
+    """
+    letters = "абвгґдеєжзиіїйклмнопрстуфхцчшщюя"
+    grid: list = random.sample(letters, 5)
+    return grid
